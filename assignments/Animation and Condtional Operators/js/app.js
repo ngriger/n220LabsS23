9b59833c899fda406bdbd09617516bc5c07d9a86
ngriger <ngriger1427@gmail.com>
2023-01-23 23:44:04 -0500

    Algorithm Check

---
+++ assignments/Animation and Condtional Operators/js/app.js
@@ -0,0 +1,25 @@
+//1 - Counter
+
+//Create variable 'x'
+//Create draw function
+    //Increase value of x
+    //Show value of x in console
+
+//2 - Puck Side
+
+//Create variable 'r'
+//Create canvas 400x300
+//Draw function
+    //Circle (mouseX, mouseY, r)
+    //if mouseX < 200, fill circle red
+    //if mouseX > 200, fill circle blue
+
+//3 - World Wrap
+
+//Create variable 'speed' (set to 5)
+//Create variable xPos (set to 0)
+//Draw function
+    //Set background color
+    //Draw Circle
+    //xPos = xPos + speed
+    //if xPos > 800, xPos = 0
