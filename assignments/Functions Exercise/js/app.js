ec01bec616fea30a832acd9d9bcc1291ef82f6cd
ngriger <ngriger1427@gmail.com>
2023-01-30 23:48:27 -0500

    Algorithm Check

---
+++ assignments/Functions Exercise/js/app.js
@@ -0,0 +1,34 @@
+// Drawing Drawer
+
+//setup canvas (800x800)
+//Draw dog - function drawDog(x, y)
+    //rectangle
+    //8 triangles for details + limbs
+    //2 lines for eyes
+//Make dog follow mouse
+    //drawDog(mouseX, mouseY)
+
+
+//Red Remover
+
+//setup canvas (500x500)
+//create function starting with red
+    //function yesRed( color(170, 200, 150))
+//set red to 0
+    //let noRed = yesRed.setRed(0)
+    //return noRed
+//draw circle
+    //fill(noRed)
+
+
+//Polar Points
+
+//setup canvas (500x500)
+//create polar points function - polarPoint(r)
+    //define x value
+    //define y value
+    //return statement
+//draw function
+    //create variable that follows polarPoint function
+    //translate(100, 100)
+    //draw circle following variable, radius 10
--- labs/lab2/js/app.js
+++ labs/lab2/js/app.js
@@ -1,4 +1,4 @@
-let cursor = (0, 0); //Change } to ), x: and y: indicators not needed, Rename "aou" to something useful
+let cursor = {x: 0, y: 0}; //Change } to ), x: and y: indicators not needed, Rename "aou" to something useful
 
 function setup() {
   createCanvas(400, 400);
@@ -6,29 +6,26 @@ function setup() {
 
 function draw() {
     background(220);
-    cursor = (mouseX, mouseY);
-
-    let p1 = (0, 0);
-    let p2 = (0, 0);
 
     if(mouseIsPressed){
-        line(cursor.x, cursor.y, mouseX, mouseY); //spelling error, "mooseY", missing comma after aou.y
+      cursor.x = mouseX;
+      cursor.y = mouseY;
     }
+
+    line(cursor.x, cursor.y, mouseX, mouseY);
   
-  let a = dist(p1, p2, mouseX, mouseY); //change "distance" to p5 variable "dist"
-  
+  let a = dist(cursor.x, cursor.y, mouseX, mouseY); 
   if(a > 120) {
     stroke("#FF0000");
-  } else { //if-else statement not formatted properly
+  } else { 
     stroke("#000000");
   }
 }
 
-function dist(p1, p2) {  //change "distance" to p5 variable "dist"
+function dist() {  
   
-  let dx = p1.x - p2.x;
-  let dy = p1.y - p2.y;
+  let dx = cursor.x - mouseX;
+  let dy = cursor.y - mouseY;
   
-  //return the distance
    Math.sqrt(dx*dx + dy*dy);
 }
