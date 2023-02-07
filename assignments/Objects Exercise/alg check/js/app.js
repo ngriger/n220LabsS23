8e0fd296ebdbaa63cfcba0fdb8fee0d69c529cbb
ngriger <ngriger1427@gmail.com>
2023-02-06 23:28:36 -0500

    algorithm check

---
+++ assignments/Objects Exercise/alg check/js/app.js
@@ -0,0 +1,32 @@
+//Data-driven display w/ function
+
+//create object with properties
+    //width, height, color, position
+//draw function
+    //use object.width, object.height, object.color, etc
+//function using object argument
+    //function drawObject(object)
+    //draw the object to the screen
+
+
+//Reverse Tennis
+
+//create first object (tall rect)
+    //properties x y width height
+    //x property = left side of screen
+//create second object (tall rect)
+    //properties x y width height
+    //x property = right side of screen
+//draw function
+    //draw both rectangles
+    //if statement - up key, one up one down
+        //do this by changing y properties !
+    //if statement - down key, other one up one down
+
+
+//Bouncing Ball / World Wrap Refractor
+
+//create ball object
+    //properties size color velocity
+//animate ball moving to the right of the screen
+//if statement to reset ball when done
