5790947583349c32dde07249511265ed013d8019
ngriger <ngriger1427@gmail.com>
2023-02-20 23:32:42 -0500

    algorithm check

---
+++ assignments/Loops and Array Exercises/algorithm check/app.js
@@ -0,0 +1,31 @@
+//1- Loop, but big
+
+//Create div in HTML
+//create array of 1000, 2000, 3000, 4000, 5000
+//for loop
+    //changes div to display numbers for value of i, set i to array
+
+
+//2- Beep Bop
+
+//create div in HTML
+//for loop - i < 24
+    //if statements
+    //i/3 - beep
+    //i/5 - bop
+    //i/3 and 5 - beepbop
+
+
+//3- Runner Up
+
+//divs in html
+//create array of 68, 72, 75
+//firstDiv.InnerHTML = array[1]
+//secondDiv.InnerHTML = array[2]
+
+
+//4- Favorray
+
+//Five favorite things listed as items in array (use "")
+//for loop, i = 0, i < 5, i++
+    //div.InnerHTML = i + "is one of my favorite things"
